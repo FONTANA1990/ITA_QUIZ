17f7096439abeb8c3c0fd7c80a4b8dc00fb8b8cf
FONTANA1990 <mediattamoveis@gmail.com>
2026-03-29 20:57:09 -0300

    Comit fix

---
+++ dist/server/ssr/__vite_rsc_assets_manifest.js
@@ -0,0 +1,145 @@
+export default {
+  "bootstrapScriptContent": "import(\"/assets/index-CW4ADw8l.js\")",
+  "clientReferenceDeps": {
+    "f29e6e234fea": {
+      "js": [
+        "/assets/facade__virtual_vinext-rsc-entry-CDSRDZjg.js",
+        "/assets/rolldown-runtime-COnpUsM8.js",
+        "/assets/index-CW4ADw8l.js",
+        "/assets/preload-helper-3k_mjWfE.js",
+        "/assets/framework-DGYcMqM3.js",
+        "/assets/query-BbOc3VB2.js"
+      ],
+      "css": []
+    },
+    "0deffcb8ffd7": {
+      "js": [
+        "/assets/facade__virtual_vinext-rsc-entry-CDSRDZjg.js",
+        "/assets/rolldown-runtime-COnpUsM8.js",
+        "/assets/index-CW4ADw8l.js",
+        "/assets/preload-helper-3k_mjWfE.js",
+        "/assets/framework-DGYcMqM3.js",
+        "/assets/query-BbOc3VB2.js"
+      ],
+      "css": []
+    },
+    "ce76877553ca": {
+      "js": [
+        "/assets/facade__virtual_vinext-rsc-entry-CDSRDZjg.js",
+        "/assets/rolldown-runtime-COnpUsM8.js",
+        "/assets/index-CW4ADw8l.js",
+        "/assets/preload-helper-3k_mjWfE.js",
+        "/assets/framework-DGYcMqM3.js",
+        "/assets/query-BbOc3VB2.js"
+      ],
+      "css": []
+    },
+    "927fe43b75aa": {
+      "js": [
+        "/assets/facade__virtual_vinext-rsc-entry-CDSRDZjg.js",
+        "/assets/rolldown-runtime-COnpUsM8.js",
+        "/assets/index-CW4ADw8l.js",
+        "/assets/preload-helper-3k_mjWfE.js",
+        "/assets/framework-DGYcMqM3.js",
+        "/assets/query-BbOc3VB2.js"
+      ],
+      "css": []
+    },
+    "1b003c57753d": {
+      "js": [
+        "/assets/facade__virtual_vinext-rsc-entry-CDSRDZjg.js",
+        "/assets/rolldown-runtime-COnpUsM8.js",
+        "/assets/index-CW4ADw8l.js",
+        "/assets/preload-helper-3k_mjWfE.js",
+        "/assets/framework-DGYcMqM3.js",
+        "/assets/query-BbOc3VB2.js"
+      ],
+      "css": []
+    },
+    "b1a19a4e637b": {
+      "js": [
+        "/assets/facade__virtual_vinext-rsc-entry-CDSRDZjg.js",
+        "/assets/rolldown-runtime-COnpUsM8.js",
+        "/assets/index-CW4ADw8l.js",
+        "/assets/preload-helper-3k_mjWfE.js",
+        "/assets/framework-DGYcMqM3.js",
+        "/assets/query-BbOc3VB2.js"
+      ],
+      "css": []
+    },
+    "73d7a23e5015": {
+      "js": [
+        "/assets/facade__virtual_vinext-rsc-entry-CDSRDZjg.js",
+        "/assets/rolldown-runtime-COnpUsM8.js",
+        "/assets/index-CW4ADw8l.js",
+        "/assets/preload-helper-3k_mjWfE.js",
+        "/assets/framework-DGYcMqM3.js",
+        "/assets/query-BbOc3VB2.js"
+      ],
+      "css": []
+    },
+    "02ca88934bb2": {
+      "js": [
+        "/assets/facade__virtual_vinext-rsc-entry-CDSRDZjg.js",
+        "/assets/rolldown-runtime-COnpUsM8.js",
+        "/assets/index-CW4ADw8l.js",
+        "/assets/preload-helper-3k_mjWfE.js",
+        "/assets/framework-DGYcMqM3.js",
+        "/assets/query-BbOc3VB2.js"
+      ],
+      "css": []
+    },
+    "5a4d3fe72cb3": {
+      "js": [
+        "/assets/facade__virtual_vinext-rsc-entry-CDSRDZjg.js",
+        "/assets/rolldown-runtime-COnpUsM8.js",
+        "/assets/index-CW4ADw8l.js",
+        "/assets/preload-helper-3k_mjWfE.js",
+        "/assets/framework-DGYcMqM3.js",
+        "/assets/query-BbOc3VB2.js"
+      ],
+      "css": []
+    },
+    "da1952991006": {
+      "js": [
+        "/assets/facade__virtual_vinext-rsc-entry-CDSRDZjg.js",
+        "/assets/rolldown-runtime-COnpUsM8.js",
+        "/assets/index-CW4ADw8l.js",
+        "/assets/preload-helper-3k_mjWfE.js",
+        "/assets/framework-DGYcMqM3.js",
+        "/assets/query-BbOc3VB2.js"
+      ],
+      "css": []
+    },
+    "36132fdd4942": {
+      "js": [
+        "/assets/facade__virtual_vinext-rsc-entry-CDSRDZjg.js",
+        "/assets/rolldown-runtime-COnpUsM8.js",
+        "/assets/index-CW4ADw8l.js",
+        "/assets/preload-helper-3k_mjWfE.js",
+        "/assets/framework-DGYcMqM3.js",
+        "/assets/query-BbOc3VB2.js"
+      ],
+      "css": []
+    },
+    "c8312bba060b": {
+      "js": [
+        "/assets/facade__virtual_vinext-rsc-entry-CDSRDZjg.js",
+        "/assets/rolldown-runtime-COnpUsM8.js",
+        "/assets/index-CW4ADw8l.js",
+        "/assets/preload-helper-3k_mjWfE.js",
+        "/assets/framework-DGYcMqM3.js",
+        "/assets/query-BbOc3VB2.js"
+      ],
+      "css": []
+    }
+  },
+  "serverResources": {
+    "src/app/layout.tsx": {
+      "js": [],
+      "css": [
+        "/assets/index-CnRZ32qA.css"
+      ]
+    }
+  }
+}
